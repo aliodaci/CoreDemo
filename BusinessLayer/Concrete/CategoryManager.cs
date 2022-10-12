b7cd08eb50bd2a3bc651946b4f4f5bfdbe9e90bd
Ali <Ali@IC-HW-NB1173>
2022-10-12 13:28:06 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,48 @@
+﻿
+using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository _categoryRepository;
+
+        public CategoryManager(EfCategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            _categoryRepository.Insert(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            _categoryRepository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            _categoryRepository.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return _categoryRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return _categoryRepository.GetAll();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,57 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        public void AddCategory(Blog blog)
+        {
+            using (var c=new ContextDb())
+            {
+                c.Add(blog);
+                c.SaveChanges();
+            }
+        }
+
+        public void DeleteCategory(Blog blog)
+        {
+            using (var c = new ContextDb())
+            {
+                c.Remove(blog);
+                c.SaveChanges();
+            }
+        }
+
+        public Blog GetById(int id)
+        {
+            using (var c = new ContextDb())
+            {
+                return c.Blogs.Find(id);
+            }
+        }
+
+        public List<Blog> ListAllGetCategory()
+        {
+            using (var c = new ContextDb())
+            {
+                return c.Blogs.ToList();
+            }
+        }
+
+        public void UpdateCategory(Blog blog)
+        {
+            using (var c = new ContextDb())
+            {
+                c.Update(blog);
+                c.SaveChanges();
+            }
+        }
+    }
+}
